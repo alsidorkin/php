2a9de50cfa0a7e0bedf1b341132aa180a6c9a3b3
aleksandr sidorkin <sanyok1234@rambler.ru>
2024-03-20 12:28:39 +0200

    task1

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php   
-$data = file_get_contents('https://dog.ceo/api/breeds/image/random');
-$result= json_decode($data, true);
-?> 
 
-<img src="<?=$result['message']?>" alt="">
+include __DIR__ . '/vendor/autoload.php';
+
+include "src/api/random-animal.phtml";
+
+
